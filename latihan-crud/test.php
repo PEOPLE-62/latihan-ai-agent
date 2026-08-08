31ecc04a44b3fcde4f582b2cef098bda011c4fde
Eka Putra Nurdin Samantani <acerr7vj0d2n00@outlook.com>
2026-08-08 19:26:23 +0700

    menambahkan fitur status umur

---
+++ latihan-crud/test.php
@@ -0,0 +1,13 @@
+<?php
+
+$nama = "Eka Putra";
+$umur = 15;
+
+echo "Nama: $nama";
+echo "Umur: $umur";
+
+if ($umur >= 18) {
+    echo "Status: Dewasa";
+} else {
+    echo "Status: Belum Dewasa";
+}
